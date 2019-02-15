5a8db46d4c679b26f6596afd689f4a6fd84d5117
Malcolm Jones <malcolm@adobe.com>
2019-02-15 05:58:16 +0000

    wip: working version with supervisord

--- containers/fluentd-elasticsearch/run.sh
+++ containers/fluentd-elasticsearch/run.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+
+# NOTE: Fix issue where mantra hides errors to stderr
+# SOURCE: https://stackoverflow.com/questions/3173131/redirect-copy-of-stdout-to-log-file-from-within-bash-script-itself
+# Without this, only stdout would be captured - i.e. your
+# log file would not contain any error messages.
+# SEE (and upvote) the answer by Adam Spiers, which keeps STDERR
+# as a separate stream - I did not want to steal from him by simply
+# adding his answer to mine.
+exec 2>&1
 
 # Copyright 2017 The Kubernetes Authors.
 #
@@ -40,4 +50,4 @@ mkdir -p /var/log/journal
 # fi
 
 
-supervisord -n -c /etc/supervisor/supervisord.conf
+exec supervisord -n -c /etc/supervisor/supervisord.conf
