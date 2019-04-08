41fb0dac4d78d32e12c2a3ce9f8deacbf4395f35
Malcolm Jones <bossjones@theblacktonystark.com>
2019-04-07 22:38:55 -0400

    Feature: rsyslog container

--- bin/send_to_fluent_in_udp.rb
+++ bin/send_to_fluent_in_udp.rb
@@ -14,8 +14,14 @@ puts "Host: #{ARGV[0]}"
 us = UDPSocket.open
 sa = Socket.pack_sockaddr_in(5140, ARGV[0])
 
+d = %x( env LANG=us_US.UTF-8 date "+%b %d %H:%M:%S" )
+h = %x( hostname )
+pid = Process.pid
+
+msg = "<150>#{d} #{h} syslog-netcat-test[#{pid}]: testing baby"
+
 # This example uses json payload.
 # In in_udp configuration, need to configure "@type json" in "<parse>"
-us.send('{"k":"v1"}', 0, sa)
-us.send('{"k":"v2"}', 0, sa)
+us.send(msg, 0, sa)
+# us.send('{"k":"v2"}', 0, sa)
 us.close
