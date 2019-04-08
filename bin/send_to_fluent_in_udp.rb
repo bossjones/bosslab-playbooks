3fd370c7532e1f4a173b2795cd4f265ef665f2f3
Malcolm Jones <bossjones@theblacktonystark.com>
2019-04-07 22:48:33 -0400

    Feature: rsyslog container

--- bin/send_to_fluent_in_udp.rb
+++ bin/send_to_fluent_in_udp.rb
@@ -9,6 +9,30 @@ if ARGV.length < 1
   exit
 end
 
+# SOURCE: https://dzone.com/articles/send-custom-udp-packets-ruby
+UDP_RECV_TIMEOUT = 3  # seconds
+
+def q2cmd(server_addr, server_port, cmd_str)
+  resp, sock = nil, nil
+  begin
+   cmd = "\377\377\377\377#{cmd_str}\0"
+    sock = UDPSocket.open
+    sock.send(cmd, 0, server_addr, server_port)
+    resp = if select([sock], nil, nil, UDP_RECV_TIMEOUT)
+      sock.recvfrom(65536)
+    end
+    if resp
+      resp[0] = resp[0][4..-1]  # trim leading 0xffffffff
+    end
+  rescue IOError, SystemCallError
+  ensure
+    sock.close if sock
+  end
+  resp ? resp[0] : nil
+end
+
+
+
 puts "Host: #{ARGV[0]}"
 
 us = UDPSocket.open
@@ -24,6 +48,16 @@ puts " [msg]: #{msg}".strip
 
 # This example uses json payload.
 # In in_udp configuration, need to configure "@type json" in "<parse>"
-us.send(msg, 0, sa)
+us.send("#{msg}", 0, sa)
 # us.send('{"k":"v2"}', 0, sa)
 us.close
+
+
+# your firewall has to allow communication with IP address 67.19.248.74 (port 27912)
+#server, port, cmd = *ARGV
+server = "#{ARGV[0]}"
+port = 5140
+cmd = "#{msg}"
+
+result = q2cmd(server, port, cmd)
+puts result
