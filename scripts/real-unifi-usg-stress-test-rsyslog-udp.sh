0866f57152aa9bc2ed0e80e7f08a216c8f872b8a
Malcolm Jones <bossjones@theblacktonystark.com>
2019-04-10 20:41:48 -0400

    Feature: pvc grok

--- scripts/real-unifi-usg-stress-test-rsyslog-udp.sh
+++ scripts/real-unifi-usg-stress-test-rsyslog-udp.sh
@@ -7,8 +7,8 @@ PATH_TO_MANIFEST="./vars/clear/${_CLUSTER}_manifest.yml"
 # domain_root: borglab.scarlettlab.home
 DNS_DOMAIN=$(grep '^domain_root:' ${PATH_TO_MANIFEST} | cut -d\: -f2 | awk '{print $1}')
 
-for i in {0..200}; do syslog-netcat-test-udp rsyslog-centralized.${DNS_DOMAIN} 6160; done
+# for i in {0..200}; do syslog-netcat-test-udp rsyslog-centralized.${DNS_DOMAIN} 6160; done
 
 source ./scripts/log_fixtures/usg-log-fixtures.sh
 
-for i in "${fake_logs_security_gateway[@]}"; do echo "$i"; done
+for i in "${fake_logs_security_gateway[@]}"; do syslog-netcat-test-udp rsyslog-centralized.${DNS_DOMAIN} 6160 "${i}"; done
