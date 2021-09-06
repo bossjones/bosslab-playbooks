e9590b10d76a8fc85b1158816a73425e15921e2a
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-06 18:22:26 -0400

    wip: k3s vmware lab, ens33

--- scripts/look-at-this-kubernetes-vagrant-install.sh
+++ scripts/look-at-this-kubernetes-vagrant-install.sh
@@ -129,7 +129,7 @@ echo ""
 echo "Your dashboard url is:"
 
 DASHBOARDPORT=`kubectl get services --all-namespaces | grep kubernetes-dashboard | awk '{print $6}' | cut -f 2 -d ':' | cut -f 1 -d '/'`
-IPADDR=`ifconfig enp0s8 | grep mask | awk '{print $2}'`
+IPADDR=`ifconfig ens33 | grep mask | awk '{print $2}'`
 
 echo ""
 echo " https://$IPADDR:$DASHBOARDPORT"
